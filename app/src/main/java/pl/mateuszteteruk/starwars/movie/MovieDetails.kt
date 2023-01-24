fdcde024c8537868632158a14d7af489df4010ae
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2023-01-24 14:54:34 +0100

    Fix PlayerView in landscape mode

--- app/src/main/java/pl/mateuszteteruk/starwars/movie/MovieDetails.kt
+++ app/src/main/java/pl/mateuszteteruk/starwars/movie/MovieDetails.kt
@@ -1,5 +1,6 @@
 package pl.mateuszteteruk.starwars.movie
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
@@ -14,6 +15,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -70,8 +72,9 @@ private fun MovieContent(
                 }
             },
             modifier = Modifier
+                .background(Color.Black)
                 .fillMaxWidth()
-                .aspectRatio(16 / 9f),
+                .aspectRatio(matchHeightConstraintsFirst = true, ratio = 16 / 9f),
             update = {
                 when (lifecycle) {
                     Lifecycle.Event.ON_PAUSE -> {
