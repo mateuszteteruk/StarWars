3c989085f3e177590ce76c5a174f2228cd34d3cb
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2023-01-24 11:58:55 +0100

    Play BigBuckBunny from the remote for testing purposes

--- app/src/main/java/pl/mateuszteteruk/starwars/movie/VideoPlayerModule.kt
+++ app/src/main/java/pl/mateuszteteruk/starwars/movie/VideoPlayerModule.kt
@@ -1,6 +1,8 @@
 package pl.mateuszteteruk.starwars.movie
 
 import android.content.Context
+import android.net.Uri
+import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import dagger.Module
@@ -19,5 +21,9 @@ object VideoPlayerModule {
     fun providesPlayer(@ApplicationContext context: Context): Player =
         ExoPlayer.Builder(context)
             .build()
+            .also {
+                val url = "https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4"
+                it.addMediaItem(MediaItem.fromUri(Uri.parse(url)))
+            }
 
 }
