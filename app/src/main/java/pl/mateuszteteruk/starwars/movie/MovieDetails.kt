3079a3a7d5e58072085d61cb1e88e9feb2c29ab6
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2023-01-24 15:10:53 +0100

    Scroll Movie Details only in portrait mode

--- app/src/main/java/pl/mateuszteteruk/starwars/movie/MovieDetails.kt
+++ app/src/main/java/pl/mateuszteteruk/starwars/movie/MovieDetails.kt
@@ -7,6 +7,9 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -16,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -45,8 +49,19 @@ private fun MovieContent(
     movieTitle: String,
     player: Player,
 ) {
+    val config = LocalConfiguration.current
+    val orientation = remember(config) { config.orientation }
+
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .then(
+                if (orientation == 1) {
+                    Modifier.verticalScroll(rememberScrollState())
+                } else {
+                    Modifier
+                }
+            ),
     ) {
 
         var lifecycle by remember {
@@ -90,9 +105,30 @@ private fun MovieContent(
         )
         Spacer(modifier = Modifier.height(16.dp))
         Text(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxWidth(),
             textAlign = TextAlign.Center,
             text = movieTitle
         )
+        (1..10).forEach {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        listOf(
+                            Color.Blue,
+                            Color.Red,
+                            Color.Gray,
+                            Color.Green,
+                            Color.Cyan,
+                            Color.Magenta,
+                        ).random()
+                    )
+                    .height(56.dp)
+                    .padding(16.dp),
+                textAlign = TextAlign.Center,
+                text = "Random text:$it"
+            )
+        }
     }
 }
