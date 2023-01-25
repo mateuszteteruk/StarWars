8a0761d8c25027f5b0576e92858a697cc043d9ec
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2023-01-25 16:17:11 +0100

    Show list of chips to select

--- app/src/main/java/pl/mateuszteteruk/starwars/movie/Movies.kt
+++ app/src/main/java/pl/mateuszteteruk/starwars/movie/Movies.kt
@@ -4,15 +4,18 @@ import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Scaffold
@@ -21,6 +24,8 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -42,12 +47,34 @@ fun Movies(
         Movie(name = "Attack of the Clones"),
         Movie(name = "Revenge of the Sith"),
     )
+    val chips = listOf(
+        "aaa",
+        "bbb",
+        "ccc",
+        "ddd",
+        "eee",
+        "fff",
+        "ggg"
+    )
+    val selectedChips = remember {
+        mutableStateOf(emptyList<String>())
+    }
     Movies(
         movies = movies,
         onMovieClick = {
             Log.d("TAG", "Movie click:$it")
             navigator.navigate(MovieDetailsDestination(movie = it.name))
-        }
+        },
+//        chips = if (selectedChips.value.isEmpty()) chips else selectedChips.value,
+        chips = chips,
+        selectedChips = selectedChips.value,
+        onSelected = {
+            if (selectedChips.value.contains(it)) {
+                selectedChips.value = emptyList()
+            } else {
+                selectedChips.value = listOf(it)
+            }
+        },
     )
 }
 
@@ -55,7 +82,10 @@ fun Movies(
 @Composable
 private fun Movies(
     movies: List<Movie>,
+    chips: List<String>,
+    selectedChips: List<String>,
     onMovieClick: (Movie) -> Unit,
+    onSelected: (String) -> Unit,
 ) {
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
 
@@ -73,6 +103,14 @@ private fun Movies(
                 .padding(it),
             verticalArrangement = Arrangement.spacedBy(16.dp),
             content = {
+                item {
+                    ChipsGroup(
+                        items = chips,
+                        selectedItems = selectedChips,
+                        onSelected = onSelected,
+                    )
+                }
+                item { Spacer(modifier = Modifier.height(16.dp)) }
                 items(
                     items = movies,
                     key = { it.name },
@@ -133,5 +171,29 @@ private fun CollapsingTopBar(
         },
         scrollBehavior = scrollBehavior,
         modifier = Modifier,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ChipsGroup(
+    items: List<String>,
+    selectedItems: List<String>,
+    onSelected: (String) -> Unit,
+) {
+    LazyRow(
+        modifier = Modifier.fillMaxWidth(),
+        content = {
+            items(items = items) {
+                FilterChip(
+                    modifier = Modifier.padding(8.dp),
+                    selected = selectedItems.contains(it),
+                    onClick = {
+                        onSelected(it)
+                    },
+                    label = { Text(text = it) }
+                )
+            }
+        },
     )
 }
